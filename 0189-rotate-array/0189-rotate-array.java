c84fcec38522a1befaca55ea7c0d9f4b95aa56d9
Situ Kumari <situkumari8130@gmail.com>
2026-03-28 12:58:34 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.java
+++ 0189-rotate-array/0189-rotate-array.java
@@ -4,17 +4,15 @@ class Solution {
         k = k % n;
        int[] temp = new int[k];
     
-
-    
         for(int i = 0; i<k;i++){
-          temp[i] = a[n-k+i];
+          temp[i] = a[n-k+i];        //temp = [5,6,7]
 
         }
-        for(int i = n-k-1;i>=0;i--){
+        for(int i = n-k-1;i>=0;i--){  //After shift:  // [1,2,3,4,1,2,3,4]
             a[i+k] = a[i];
         }
         for(int i=0;i<k;i++){
-            a[i] = temp[i];
+            a[i] = temp[i];  //ab aage ke elements ko 'temp' se replace krdo!
            
         }
     }
