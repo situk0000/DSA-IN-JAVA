fd136ccdecfd70b5a464535e4d9f7790f02f125c
Situ Kumari <situkumari8130@gmail.com>
2026-04-13 23:55:57 +0530

    Time: 3 ms (73.99%), Space: 53.6 MB (71.33%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] score = new int[n + 1];
+
+        for (int[] relation : trust) {
+            int a = relation[0];
+            int b = relation[1];
+
+            score[a]--; // a trusts someone
+            score[b]++; // b is trusted by someone
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (score[i] == n - 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
